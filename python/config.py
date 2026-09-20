491c43dbc53ee765f26e4a2c1a58d99d42ead20e
Mariana Gomes <marianagandrades@gmail.com>
2026-09-20 16:52:26 -0300

    Update config.py

--- python/config.py
+++ python/config.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 
-
 DATA_DIR = PROJECT_DIR / "data"
 PROCESSED_DIR = DATA_DIR / "processed"
 OUTPUT_DIR = PROJECT_DIR / "outputs"
